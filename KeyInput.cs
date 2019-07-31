4fcdd04eacc329a9af8e651b1da7015e935c2f39
Zpinx <t-pedersen@live.no>
2019-08-01 00:28:31 +0200

    Add files via upload

---
+++ KeyInput.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace Csharp310719
+{
+    public class StopWatch
+    {
+        public static void _stopWatch()
+        {
+            var stopWatch = new Stopwatch();
+
+            start:
+            stopWatch.Start();
+            Console.WriteLine("\ntime started..\npress Enter to Stop");
+            Console.ReadKey();
+            var ts = stopWatch.Elapsed;
+            stopWatch.Stop();
+            Console.WriteLine("\ntime stopped...Press Enter to start again");
+            var elapsedTime = $"{ts.Hours:00}:{ts.Minutes:00}:{ts.Seconds:00}.{ts.Milliseconds / 10:00}";
+            Console.WriteLine("RunTime " + elapsedTime);
+            Console.ReadKey();
+            goto start;
+            
+        }
+    }
+}
+
+
+
+
+                
+                    
+
+
+
+
+
+                    
+  
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Transactions;
+
+namespace Csharp310719
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+            Console.WriteLine("Press the Enter Key to begin");
+            Console.ReadKey();
+            StopWatch._stopWatch();
+            
+
+
+
+        }
+
+       
+    }
+
+   
+}
